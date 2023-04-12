b88e143e4834882dd1559b5279e1ab82daad7f32
Jeremy Magland <jeremy.magland@gmail.com>
2023-04-12 13:50:33 -0400

    comparison with truth

--- core/execute_single_sorting.py
+++ core/execute_single_sorting.py
@@ -9,14 +9,14 @@ from core.config_classes import SpikeSortingTestsConfig, SortingConfig
 from core.capture_console_output import capture_console_output, setup_logger
 
 
-def execute_single_sorting(config: SpikeSortingTestsConfig, sorting_config: SortingConfig):
-    sorter = next((s for s in config.sorters if s.id == sorting_config.sorter), None)
+def execute_single_sorting(config: SpikeSortingTestsConfig, sorting_extractor: SortingConfig):
+    sorter = next((s for s in config.sorters if s.id == sorting_extractor.sorter), None)
     if sorter is None:
-        print(f'Sorter {sorting_config.sorter} not found in config. Skipping.')
+        print(f'Sorter {sorting_extractor.sorter} not found in config. Skipping.')
         return
-    recording = next((r for r in config.recordings if r.id == sorting_config.recording), None)
+    recording = next((r for r in config.recordings if r.id == sorting_extractor.recording), None)
     if recording is None:
-        print(f'Recording {sorting_config.recording} not found in config. Skipping.')
+        print(f'Recording {sorting_extractor.recording} not found in config. Skipping.')
         return
 
     recording_folder = f'output/recordings/{recording.id}'
@@ -47,23 +47,30 @@ def execute_single_sorting(config: SpikeSortingTestsConfig, sorting_config: Sort
         kwargs['job_kwargs'] = {'n_jobs': 4}
     logger = setup_logger(f'{sorting_folder}/output.log')
     with capture_console_output(logger):
-        sorting_config = ss.run_sorter(
-            sorter.algorithm,
-            recording=recording_extractor,
-            output_folder=sorting_folder + '/output',
-            **kwargs
-        )
+        if sorter.type == 'spikeinterface':
+            sorting_extractor = ss.run_sorter(
+                sorter.algorithm,
+                recording=recording_extractor,
+                output_folder=sorting_folder + '/output',
+                **kwargs
+            )
+        elif sorter.type == 'truth':
+            if sorter.algorithm != 'truth':
+                raise Exception(f'unexpected truth algorithm: {sorter.algorithm}')
+            sorting_extractor = si.NpzSortingExtractor(f'{recording_folder}/sorting_true/sorting.npz')
+        else:
+            raise Exception(f'Unknown sorter type: {sorter.type}')
 
     print('Writing sorting')
-    se.NpzSortingExtractor.write_sorting(sorting_config, f'{sorting_folder}/sorting.npz')
+    se.NpzSortingExtractor.write_sorting(sorting_extractor, f'{sorting_folder}/sorting.npz')
 
     print('Writing sorting info')
     sorting_info = {
         'timestamp': time.time(),
-        'num_units': len(sorting_config.unit_ids)
+        'num_units': len(sorting_extractor.unit_ids)
     }
     with open(f'{sorting_folder}/sorting_info.json', 'w') as f:
         json.dump(sorting_info, f, indent=4)
 
     print('Unit IDs:')
-    print(sorting_config.unit_ids)
+    print(sorting_extractor.unit_ids)
